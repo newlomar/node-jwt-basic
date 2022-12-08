76034c12e69dcbda9826989233ee93fdae9dd394
Newton Lomar <newtonlomar16@gmail.com>
2022-12-08 09:55:20 -0300

    adding space

--- errors/unauthenticated.js
+++ errors/unauthenticated.js
@@ -1,5 +1,6 @@
 const CustomAPIError = require('./custom-error')
 const { StatusCodes } = require('http-status-codes')
+
 class UnauthenticatedError extends CustomAPIError {
     constructor(message) {
       super(message)
