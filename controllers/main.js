cc3e98c032bca518deadce1964d795d67b1100bc
Newton Lomar <newtonlomar16@gmail.com>
2022-12-08 09:24:49 -0300

    moving response to try block and adding custom username response message

--- controllers/main.js
+++ controllers/main.js
@@ -30,16 +30,16 @@ const dashboard = async (req, res) => {
     
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        
+        const luckyNumber = Math.floor(Math.random() * 100)
+        
+        res.status(200).json({
+            msg: `Hello, ${decoded.username}`, 
+            secret: `Here is your authorized data, your lucky number is ${luckyNumber}`
+        })
     } catch (error) {
         throw new CustomAPIError('Not authorized to access this route', 401)
     }
-
-    const luckyNumber = Math.floor(Math.random() * 100)
-    res.status(200).json(
-        {
-            msg: `Hello, John Doe`, 
-            secret: `Here is your authorized data, your lucky number is ${luckyNumber}`}
-        )
 }
 
 module.exports = {
