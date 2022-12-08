964009679d51fb5809725aaf6ff705d934030b57
Newton Lomar <newtonlomar16@gmail.com>
2022-12-08 09:47:44 -0300

    refactoring auth

--- controllers/main.js
+++ controllers/main.js
@@ -19,27 +19,15 @@ const login = async (req, res) => {
     res.status(200).json({msg: 'user created', token})
 }
 
-const dashboard = async (req, res) => {
-    const authHeader = req.headers.authorization;
+const dashboard = async (req, res) => {    
+    console.log(req.user)
 
-    if (!authHeader || !authHeader.startsWith('Bearer ')) {
-        throw new CustomAPIError('No token provided', 401)
-    }
-
-    const token = authHeader.split(' ')[1]
-    
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+    const luckyNumber = Math.floor(Math.random() * 100)
         
-        const luckyNumber = Math.floor(Math.random() * 100)
-        
-        res.status(200).json({
-            msg: `Hello, ${decoded.username}`, 
-            secret: `Here is your authorized data, your lucky number is ${luckyNumber}`
-        })
-    } catch (error) {
-        throw new CustomAPIError('Not authorized to access this route', 401)
-    }
+    res.status(200).json({
+        msg: `Hello, ${req.user.username}`, 
+        secret: `Here is your authorized data, your lucky number is ${luckyNumber}`
+    })
 }
 
 module.exports = {
