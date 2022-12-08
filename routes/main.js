2257c31b910b43d53cd569991bb42bebfcccf312
Newton Lomar <newtonlomar16@gmail.com>
2022-12-08 09:30:47 -0300

    adding authentication middleware to dashboard route

--- routes/main.js
+++ routes/main.js
@@ -3,7 +3,9 @@ const router = express.Router()
 
 const { login, dashboard } = require('../controllers/main')
 
-router.route('/dashboard').get(dashboard)
+const authenticationMiddleware = require('../middleware/auth')
+
+router.route('/dashboard').get(authenticationMiddleware, dashboard)
 router.route('/login').post(login)
 
 module.exports = router
